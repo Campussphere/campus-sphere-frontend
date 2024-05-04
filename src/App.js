99e6970ec741efd89b0bbdf8bd9e49252f540b42
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2024-05-04 09:48:05 +0530

    Added React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
+import CreateUser from "./pages/user-registration/CreateUser"
+import RequesReset from "./pages/auth/RequestReset";
+import ResetPassword from "./pages/auth/ResetPassword";
+import Dashboard from "./pages/dashboard";
+import DepartmentView from "./pages/admin/DepartmentViews";
+import CreateDepartment from "./pages/admin/CreateDepartment";
 import './App.css';
 
 function App() {
@@ -8,6 +14,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/create-user" element={<CreateUser />} />
+        <Route path="/reset-password" element={<RequesReset />} />
+        <Route path="/reset-password/:resetToken" element={<ResetPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/departments" element={<DepartmentView />} />
+        <Route path="/create-dept" element={<CreateDepartment />} />
       </Routes>
     </BrowserRouter>
   );
